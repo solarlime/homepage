a6424294999b9a40d3b328c92be2389e55d5531d
solarlime <solarlime@outlook.com>
2022-07-20 02:27:16 +0300

    Made responsiveness better with clamp()

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,17 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
+
+/**
+ * A listener for resizing. Works good for mobiles
+ */
+window.addEventListener('resize', () => {
+  const vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
+window.dispatchEvent(new Event('resize'));
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
