d4f96abb016d43416ff73baf1724fc1ba629e41d
solarlime <solarlime@outlook.com>
2022-07-26 00:40:05 +0300

    Added some raw layout to a main page

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,20 +1,49 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Routes, Route, useParams } from 'react-router-dom';
 import styles from './Main.module.sass';
 import NotFound from './NotFound/NotFound';
 import About from './About/About';
 import Projects from './Projects/Projects';
 import Project from './Projects/Project/Project';
+import { ThemeContext } from '../../Theme';
 
-function Intro() {
+export function Intro() {
+  const { theme } = useContext(ThemeContext);
+
   return (
-    <article>
-      Introduction
+    <article
+      className={styles.base}
+      style={{ color: theme.color, backgroundColor: theme.backgroundColor }}
+    >
+      <section id="me" className={`${styles.intro} ${styles.base__item}`}>
+        <h1 className={styles.intro__title}>
+          <p className={styles.base__item__title}>
+            Hi!
+          </p>
+          <p className={styles.intro__title_subtitle}>{`I am ${process.env.REACT_APP_ME}`}</p>
+        </h1>
+        <picture className={styles.intro__image}>
+          <img
+            sizes="100w"
+            srcSet={`${process.env.REACT_APP_FILES}/me320.jpg 320w, 
+            ${process.env.REACT_APP_FILES}/me640.jpg 640w, 
+            ${process.env.REACT_APP_FILES}/me1280.jpg 1280w`}
+            src={`${process.env.REACT_APP_FILES}/me.jpg`}
+            alt="Me"
+          />
+        </picture>
+        <blockquote className={styles.intro__text}>
+          I am inspired by different patterns of industrial design.
+          However, the better thing is an opportunity to animate them,
+          to provide an ability to interact with them.
+          At this moment, I feel that I have really created something new.
+        </blockquote>
+      </section>
     </article>
   );
 }
 
-function AboutOrNot() {
+export function AboutOrNot() {
   const params = useParams();
   if (params.please === process.env.REACT_APP_PLEASE) {
     return <About />;
