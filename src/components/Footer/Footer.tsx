359a532e66d82d69bd9269bed7213406278f03c7
solarlime <solarlime@outlook.com>
2022-07-27 23:04:51 +0300

    Removed obsolete button

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -11,13 +11,10 @@ function Footer() {
       className={styles.footer}
       style={{ color: theme.color, backgroundColor: theme.backgroundColor }}
     >
-      <div>
-        <button className={`${styles.button} ${styles.footer__item}`} type="button">Menu</button>
-        <nav className={styles.footer__item}>
-          <Link className={`${styles.button} ${styles['button-link']} ${styles.navigation__item}`} to="/">Main</Link>
-          <Link className={`${styles.button} ${styles['button-link']} ${styles.navigation__item}`} to="/projects">Projects</Link>
-        </nav>
-      </div>
+      <nav className={styles.footer__item}>
+        <Link className={`${styles.button} ${styles['button-link']} ${styles.navigation__item}`} to="/">Main</Link>
+        <Link className={`${styles.button} ${styles['button-link']} ${styles.navigation__item}`} to="/projects">Projects</Link>
+      </nav>
     </footer>
   );
 }
