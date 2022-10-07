b3fbfa8342424c51e1cd0fed40efd7a5da18d97f
solarlime <solarlime@outlook.com>
2022-10-07 03:09:11 +0300

    Bugfix: there were difficulties in defining a scrolling height. Now it works correctly (as I hope 😄)

--- src/components/Main/Projects/Project/Project.tsx
+++ src/components/Main/Projects/Project/Project.tsx
@@ -8,8 +8,12 @@ import { ThemeContext } from '../../../../Theme';
 import { LanguageContext } from '../../../../Language';
 import { getContent, PageComponent } from '../../../Content/getContent';
 
-function Image(props: { kebabedProjectName: string, projectName: string, styleProp: string }) {
-  const { kebabedProjectName, projectName, styleProp } = props;
+function Image(props: {
+  kebabedProjectName: string, projectName: string, styleProp: string, callback?: Function
+}) {
+  const {
+    kebabedProjectName, projectName, styleProp, callback,
+  } = props;
 
   return (
     <img
@@ -20,9 +24,14 @@ function Image(props: { kebabedProjectName: string, projectName: string, stylePr
             ${process.env.REACT_APP_FILES}/projects/${kebabedProjectName}1280.jpg 1280w`}
       src={`${process.env.REACT_APP_FILES}/projects/${kebabedProjectName}.jpg`}
       alt={`${projectName}`}
+      onLoad={() => { if (callback) callback(); }}
     />
   );
 }
+
+Image.defaultProps = {
+  callback: undefined,
+};
 
 function Project() {
   const location = useLocation();
@@ -42,7 +51,7 @@ function Project() {
   };
   const [visibility, setVisibility] = useState(initVisibility);
   const isDesktop = (document.documentElement.clientWidth > 600);
-  const delay = 400;
+  const delay = 800;
 
   const transform = () => {
     const space = scrollingSpaceRef.current! as HTMLElement;
@@ -54,24 +63,29 @@ function Project() {
     const pictureFix = (picture.scrollWidth - image.scrollWidth) / 2;
     const breakpoints = {
       initialScroll: picture.offsetHeight,
-      // eslint-disable-next-line max-len
       pictureHalf: picture.offsetHeight + picture.scrollWidth - space.offsetWidth / 2 - pictureFix,
       pictureFull: picture.offsetHeight + picture.scrollWidth - pictureFix,
     };
+    // Here a picture begins scrolling
     if (space.scrollTop > breakpoints.initialScroll) {
-      // Here a picture begins scrolling
       if (visibility.header === 'visible') setVisibility((oldVisibility) => ({ ...oldVisibility, header: 'hidden' }));
       picture.style.transform = `matrix(1, 0, 0, 1, ${-space.scrollTop + breakpoints.initialScroll}, 0)`;
+      // Behaviour differs on desktops / tablets and mobiles.
+      // There's no enough space to put both aim blocks together
       if (space.scrollTop > breakpoints.pictureHalf && isDesktop) {
+        // Show the original aim & fix the picture
         if (visibility.original_aim === styles.disappear) {
           setVisibility((oldVisibility) => ({ ...oldVisibility, original_aim: styles.appear }));
         }
         picture.style.transform = `matrix(1, 0, 0, 1, ${-(picture.scrollWidth - space.offsetWidth / 2 - pictureFix)}, 0)`;
+        // The delay is needed to give some space for reading without a chance to scroll over
         if (space.scrollTop > breakpoints.pictureHalf + delay) {
+          // Then, continue moving
           picture.style.transform = `matrix(1, 0, 0, 1, ${-space.scrollTop + breakpoints.initialScroll + delay}, 0)`;
           aim.style.transform = `matrix(1, 0, 0, 1, ${-space.scrollTop + breakpoints.pictureHalf + delay}, 0)`;
-          // eslint-disable-next-line max-len
+          // Until the picture is gone
           if (space.scrollTop > breakpoints.pictureFull + delay) {
+            // Finally, fix aims
             aim.style.transform = `matrix(1, 0, 0, 1, ${-picture.offsetWidth / 2}, 0)`;
             if (visibility.total_aim === styles.disappear) {
               setVisibility((oldVisibility) => ({ ...oldVisibility, total_aim: styles.appear }));
@@ -82,19 +96,23 @@ function Project() {
         } else {
           aim.style.transform = 'matrix(1, 0, 0, 1, 0, 0)';
         }
-        // eslint-disable-next-line max-len
+        // Another logic is for mobiles
+        // It's needed to scroll the whole picture
       } else if (space.scrollTop > breakpoints.pictureFull && !isDesktop) {
         if (visibility.original_aim === styles.disappear) {
           setVisibility((oldVisibility) => ({ ...oldVisibility, original_aim: styles.appear }));
         }
+        // An extra pixel is added because of a visible border
         picture.style.transform = `matrix(1, 0, 0, 1, ${-(picture.scrollWidth - pictureFix + 1)}, 0)`;
-        // eslint-disable-next-line max-len
+        // Show the original aim after scrolling the picture
         if (space.scrollTop > breakpoints.pictureFull + delay) {
           picture.style.transform = `matrix(1, 0, 0, 1, ${-space.scrollTop + breakpoints.initialScroll + delay}, 0)`;
           aim.style.transform = `matrix(1, 0, 0, 1, ${-space.scrollTop + breakpoints.initialScroll + delay + picture.scrollWidth - pictureFix}, 0)`;
+          // The next step is to scroll over the first aim
           if (space.scrollTop > breakpoints.pictureFull + delay + space.offsetWidth) {
             picture.style.transform = `matrix(1, 0, 0, 1, ${-(picture.scrollWidth + space.offsetWidth - pictureFix)}, 0)`;
             aim.style.transform = `matrix(1, 0, 0, 1, ${-space.offsetWidth}, 0)`;
+            // And show the second aim
             if (visibility.total_aim === styles.disappear) {
               setVisibility((oldVisibility) => ({ ...oldVisibility, total_aim: styles.appear }));
             }
@@ -113,11 +131,9 @@ function Project() {
     }
   };
 
-  let state = location.state as ProjectsObject;
-
-  useEffect(() => {
-    const picture = pictureRef.current! as HTMLElement;
+  const detectSize = () => {
     const space = scrollingSpaceRef.current! as HTMLElement;
+    const picture = pictureRef.current! as HTMLElement;
     space.scrollTop = 0;
     if (isDesktop) {
       setHeight(
@@ -128,7 +144,9 @@ function Project() {
         picture.scrollWidth + space.offsetWidth + picture.offsetHeight + delay * 2,
       );
     }
-  }, [pictureRef.current, state]);
+  };
+
+  let state = location.state as ProjectsObject;
 
   useEffect(() => {
     getContent(language, 'project')
@@ -175,6 +193,7 @@ function Project() {
                   kebabedProjectName={state.kebabedProjectName}
                   projectName={state.projectName}
                   styleProp={styles.img}
+                  callback={detectSize}
                 />
               </picture>
             </div>
