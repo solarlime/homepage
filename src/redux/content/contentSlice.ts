bff0b9869e8fc7f72b22c486b58ddcc6c1af9218
solarlime <solarlime@outlook.com>
2024-07-04 00:07:59 +0300

    Added another content source — Vercel's one can be very slow

--- src/redux/content/contentSlice.ts
+++ src/redux/content/contentSlice.ts
@@ -1,10 +1,33 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import type {
+  BaseQueryFn, FetchArgs, FetchBaseQueryError, FetchBaseQueryMeta,
+} from '@reduxjs/toolkit/query/react';
+
+const optionedBaseQuery: BaseQueryFn<
+string | FetchArgs,
+unknown, FetchBaseQueryError,
+{},
+FetchBaseQueryMeta
+> = async (args, api, extraOptions) => {
+  const mainQuery = fetchBaseQuery({
+    baseUrl: `${import.meta.env.VITE_APP_SERVER_PUBLIC}/homepage/content`,
+  });
+
+  const fallbackQuery = fetchBaseQuery({
+    baseUrl: import.meta.env.VITE_APP_API,
+  });
+
+  try {
+    await fetch(`${import.meta.env.VITE_APP_SERVER_PUBLIC}/isServerDown`);
+    return await mainQuery(args, api, extraOptions);
+  } catch (e) {
+    return fallbackQuery(args, api, extraOptions);
+  }
+};
 
 export const contentApi = createApi({
   reducerPath: 'contentApi',
-  baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.VITE_APP_API,
-  }),
+  baseQuery: optionedBaseQuery,
   endpoints: (builder) => ({
     getContentByComponent: builder.query({
       query: ({ languageName, component }) => `/${languageName}/${component}`,
