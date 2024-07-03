140c8f38654aa22a18ddf430a2cf605ec6f77193
solarlime <solarlime@outlook.com>
2024-07-04 00:07:59 +0300

    Added another content source — Vercel's one can be very slow

--- tests/mocks/handlers.ts
+++ tests/mocks/handlers.ts
@@ -26,6 +26,7 @@ const handlers = [
     const answer = await import(`../../api/${language}/${component}`).then((res) => { const { response } = res; return response; });
     return HttpResponse.json(answer);
   }),
+  http.get('http://test.server/isServerDown', () => HttpResponse.error()),
   http.get('https://api.unsplash.com/photos/random', () => HttpResponse.json(unsplashResult)),
   http.post(import.meta.env.VITE_APP_SERVER, () => HttpResponse.error()),
 ];
